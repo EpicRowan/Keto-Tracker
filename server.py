32395cb7ae67ee2f6f796bbaa31d7deb7f1e846a
Rowan Shepherd <shepherd.rowan.k@gmail.com>
2020-06-18 07:50:12 -0700

    Getting close to successfully searching, having bug with list vs dict results

--- server.py
+++ server.py
@@ -23,109 +23,108 @@ app.jinja_env.undefined = StrictUndefined
 
 @app.route('/')
 def index():
-    """Homepage."""
-    user_id = session.get("user_id")
-    if user_id:
-        return redirect(f"/users/{user_id}")
+	"""Homepage."""
+	user_id = session.get("user_id")
+	if user_id:
+		return redirect(f"/users/{user_id}")
 
-    print(session)
-    return render_template("homepage.html")
+	print(session)
+	return render_template("homepage.html")
 
 
 @app.route('/register', methods=['GET'])
 def register_form():
-    """Show form for user signup."""
+	"""Show form for user signup."""
 
-    return render_template("register_form.html")
+	return render_template("register_form.html")
 
 
 @app.route('/register', methods=['POST'])
 def register_process():
-    """Process registration."""
+	"""Process registration."""
 
-    # Get form variables
-    email = request.form["email"]
-    password = request.form["password"]
+	# Get form variables
+	email = request.form["email"]
+	password = request.form["password"]
 
-    if User.query.filter_by(email=email).first():
-    	flash("An account with this email address already exists.")
-    	return redirect("/register")
+	if User.query.filter_by(email=email).first():
+		flash("An account with this email address already exists.")
+		return redirect("/register")
 
-    new_user = User(email=email, password=password)
+	new_user = User(email=email, password=password)
 
-    db.session.add(new_user)
-    db.session.commit()
+	db.session.add(new_user)
+	db.session.commit()
 
-    # session["user_id"] = user_id
+	# session["user_id"] = user_id
 
-    flash(f"User {email} added.")
-    return redirect(f"/login")
+	flash(f"User {email} added.")
+	return redirect(f"/login")
 
 
 @app.route('/login', methods=['GET'])
 def login_form():
-    """Show login form."""
+	"""Show login form."""
 
-    return render_template("login_form.html")
+	return render_template("login_form.html")
 
 
 @app.route('/login', methods=['POST'])
 def login_process():
-    """Process login."""
+	"""Process login."""
 
-    # Get form variables
-    email = request.form["email"]
-    password = request.form["password"]
+	# Get form variables
+	email = request.form["email"]
+	password = request.form["password"]
 
-    user = User.query.filter_by(email=email).first()
+	user = User.query.filter_by(email=email).first()
 
-    if not user:
-        flash("No such user", 'error')
-        return redirect("/login")
+	if not user:
+		flash("No such user", 'error')
+		return redirect("/login")
 
-    if user.password != password:
-        flash("Incorrect password", 'error')
-        return redirect("/login")
+	if user.password != password:
+		flash("Incorrect password", 'error')
+		return redirect("/login")
 
-    session["user_id"] = user.user_id
+	session["user_id"] = user.user_id
 
-    flash("Logged in")
-    return redirect(f"/users/{user.user_id}")
+	flash("Logged in")
+	return redirect(f"/users/{user.user_id}")
 
 
 @app.route("/users/<int:user_id>")
 def user_detail(user_id):
-    """Show user's page"""
+	"""Show user's page"""
 
-    user = User.query.get(user_id)
-    meals = Meal.query.filter_by(user_id=user_id).all()
+	user = User.query.get(user_id)
+	meals = Meal.query.filter_by(user_id=user_id).all()
 
-    return render_template("user.html", user_id=user_id, user=user, meals=meals)
+	return render_template("user.html", user_id=user_id, user=user, meals=meals)
 
 
 @app.route('/users/<int:user_id>/new', methods=['GET'])
 def new_food_entry_form(user_id):
-    """Show form for user's new entry."""
-    return render_template("entry.html", user_id=user_id)
+	"""Show form for user's new entry."""
+	return render_template("entry.html", user_id=user_id)
 
 
 @app.route('/users/<int:user_id>/new', methods=['POST'])
 def new_entry(user_id):
-    """Process new food entry."""
+	"""Process new food entry."""
 
-    user_id = session["user_id"] 
+	user_id = session["user_id"] 
+	# # Get form variables
+	# date = request.form["date"]
+	searched=request.form["searched"]
+	params = searched.replace(" ", "%20")
+	res = requests.get(f'https://api.edamam.com/api/food-database/v2/parser?ingr=params&app_id={config.app_id}&app_key={config.api_key}')
+	search_results = res.json()
+	print(search_results)
+	name = search_results["text"]
+	carbs = search_results["hints"]["food"]["nutrients"]["CHOCDF"]
 
-    # # Get form variables
-    # date = request.form["date"]
-    searched = request.form["searched"]
-
-    # new_meal = Meal(date=date, user_id=user_id)
-
-    # db.session.add(new_meal)
-    # db.session.commit()
-
-    # flash(f"Food added.")
-    return redirect(f"/search_results", searched=searched)
+	return render_template('search_results.html', name=name, carbs=carbs)
 
 @app.route('/search_results')
 def search_food(searched):
@@ -147,23 +146,23 @@ def search_food(searched):
 
 @app.route('/logout')
 def logout():
-    """Log out."""
+	"""Log out."""
 
-    del session["user_id"]
-    flash("Logged Out")
-    return redirect("/")
+	del session["user_id"]
+	flash("Logged Out")
+	return redirect("/")
 
 
 if __name__ == "__main__":
-    # We have to set debug=True here, since it has to be True at the
-    # point that we invoke the DebugToolbarExtension
-    app.debug = True
-    # make sure templates, etc. are not cached in debug mode
-    app.jinja_env.auto_reload = app.debug
+	# We have to set debug=True here, since it has to be True at the
+	# point that we invoke the DebugToolbarExtension
+	app.debug = True
+	# make sure templates, etc. are not cached in debug mode
+	app.jinja_env.auto_reload = app.debug
 
-    connect_to_db(app)
+	connect_to_db(app)
 
-    # Use the DebugToolbar
-    DebugToolbarExtension(app)
+	# Use the DebugToolbar
+	DebugToolbarExtension(app)
 
-    app.run(port=5000, host='0.0.0.0')
+	app.run(port=5000, host='0.0.0.0')
