622090e1515770f10a53ed620e5412212bea46ac
Rowan Shepherd <shepherd.rowan.k@gmail.com>
2020-07-24 17:40:24 -0700

    Set up flask routes to be tested

--- test_server.py
+++ test_server.py
@@ -4,7 +4,7 @@ from model import db, connect_to_db, example_data
 
 class TestFlaskRoutes(unittest.TestCase):
     """Test Flask routes."""
-    
+
         def setUp(self):
       """Stuff to do before every test."""
 
@@ -27,6 +27,136 @@ class TestFlaskRoutes(unittest.TestCase):
 
         result = self.client.get('/')
         self.assertEqual(result.status_code, 200)
+
+
+      # Routes to be tested
+
+    @app.route('/register', methods=['GET'])
+	def test_register_form(self):
+	"""Show form for user signup."""
+
+
+	@app.route('/register', methods=['POST'])
+	def test_register_process(self):
+	"""Process registration."""
+
+	# Get form variables
+	email = request.form["email"]
+	password = request.form["password"]
+
+	if User.query.filter_by(email=email).first():
+		flash("An account with this email address already exists.")
+		return redirect("/register")
+
+	new_user = User(email=email, password=password)
+
+	db.session.add(new_user)
+	db.session.commit()
+
+	# session["user_id"] = user_id
+
+	flash(f"User {email} added.")
+	return redirect(f"/login")
+
+
+	@app.route('/login', methods=['GET'])
+	def test_login_form(self):
+	"""Show login form."""
+
+
+	@app.route('/login', methods=['POST'])
+	def test_login_process(self):
+	"""Process login."""
+
+	# Get form variables
+	email = request.form["email"]
+	password = request.form["password"]
+
+	user = User.query.filter_by(email=email).first()
+
+	if not user:
+		flash("No such user", 'error')
+		return redirect("/login")
+
+	if user.password != password:
+		flash("Incorrect password", 'error')
+		return redirect("/login")
+
+	session["user_id"] = user.user_id
+
+	flash("Logged in")
+	return redirect(f"/users/{user.user_id}")
+
+
+	@app.route("/users/<int:user_id>")
+	def test_user_detail(self):
+	"""Show user's page"""
+
+	user = User.query.get(user_id)
+	meals = Meal.query.filter_by(user_id=user_id).all()
+
+	return render_template("user.html", user_id=user_id, user=user, meals=meals)
+
+
+	@app.route('/users/<int:user_id>/new', methods=['GET'])
+	def test_new_food_entry_form(self):
+	"""Show form for user's new entry."""
+	
+	return render_template("entry.html", user_id=user_id)
+
+
+	@app.route('/users/<int:user_id>/new', methods=['POST'])
+	def test_new_entry(self):
+	"""Process new food entry."""
+
+
+		#  Get form variables
+	user_id = session["user_id"] 
+	food = request.form["food"]
+	carbs = request.form["carbs"]
+	date = request.form["date"]
+	
+
+	new_meal = Meal(user_id=user_id,food=food, carbs=carbs, date=date)
+
+	db.session.add(new_meal)
+	db.session.commit()
+	
+		
+	return redirect(f"/users/{user_id}")
+
+	@app.route('/search')
+	def test_search(self):
+	"""Redirect to Edam food API search"""
+
+
+	return render_template('search.html')
+
+
+	@app.route('/search_results', methods=["POST"])
+	def test_search_results(self):
+	"""Search the Edam food API for food name and carb count"""
+
+	searched=request.form["searched"]
+	params = searched.replace(" ", "%20")
+	res = requests.get(f'https://api.edamam.com/api/food-database/v2/parser?ingr={params}&app_id={config.app_id}&app_key={config.api_key}')
+	search_results = res.json()
+	foods = {}
+	i = 0
+	for item in search_results.values():
+		foods.update([(search_results["hints"][i]["food"]["label"], search_results["hints"][i]["food"]["nutrients"]["CHOCDF"])])
+		i+=1
+		
+	return render_template('search_results.html', foods=foods)
+
+
+	@app.route('/logout')
+	def test_logout(self):
+	"""Log out."""
+
+	del session["user_id"]
+	flash("Logged Out")
+	return redirect("/")
 
 
 if __name__ == '__main__':
